8edffbd21b8ed9b32e4ab4b41035450616d66d1a
gayagayataiga <ryotsu.kankiti.kotikame@gmail.com>
2025-10-31 12:44:47 +0900

    try once

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ except ImportError as e:
 
 # ラズベリーパイからくる映像ストリームのURL
 # 家、TeaLab,TeaClassで全然違うので注意
-STREAM_URL = 'http://10.27.75.172:5000/video_feed'
+STREAM_URL = 'http://10.27.72.43:5000/video_feed'
 
 # --- 状態定義 ---
 # 上から順に上半身を探す -> 顔を探す -> 顔を分析・追跡
